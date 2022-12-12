c6309d11210da336238c2438cede86dac0ae1507
Krishna V <kg5300@srmist.edu.in>
2022-12-12 08:05:37 +0530

    Create sql_script.sql

---
+++ public/sql_script.sql
@@ -0,0 +1,13 @@
+CREATE TABLE ppm(
+	user_id INT AUTO_INCREMENT PRIMARY KEY,
+	username VARCHAR(255),
+	fname VARCHAR(255),
+	lname VARCHAR(255),
+	age INT,
+	eduQual VARCHAR(255),
+	email VARCHAR(255),
+	country VARCHAR(255),
+	phone VARCHAR(255),
+	website VARCHAR(255),
+	address VARCHAR(255)
+);
